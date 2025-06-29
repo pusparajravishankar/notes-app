e926ca8e6f63b7781b9c2f5640d1a6ca81a03097
pusparajravishankar <pusparajravishankar@gmail.com>
2025-06-29 22:42:21 +0530

    color changed

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,9 +5,6 @@ dotenv.config();
 
 const { PGUSER, PGPASSWORD, PGHOST, PGDATABASE } = process.env;
 
-
- // psql 'postgresql://neondb_owner:npg_jBK1QJVYTOH3@ep-crimson-dream-a8olysqq-pooler.eastus2.azure.neon.tech/neondb?sslmode=require'
- //Below configuration as to match like above format
  
 export const sql = neon(
   `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require`
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 function Navbar() {
   return (
     <>
-      <header className="w-full h-10 text-2xl bg-[#2C3639] flex justify-between px-4 items-center text-wheat">
+      <header className="w-full h-10 text-2xl bg-[#222831] flex justify-between px-4 items-center text-wheat">
         <h1>Notes</h1>
         <h2>P</h2>
       </header>
